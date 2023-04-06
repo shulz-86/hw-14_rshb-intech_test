216e90d891107e7a0589801d1bb363c615f79aca
Anastasiya Olshanskaya <shulz-86@yandex.ru>
2023-04-06 20:02:02 +0300

    fix config

--- src/test/java/config/WebDriverProvider.java
+++ src/test/java/config/WebDriverProvider.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.Configuration;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Configuration.remote;
-
 public class WebDriverProvider {
     private static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
@@ -16,11 +14,9 @@ public class WebDriverProvider {
         Configuration.browserVersion = WebDriverProvider.config.getBrowserVersion();
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
-        String remoteUrl = WebDriverProvider.config.getRemoteUrl();
+        Configuration.remote = config.getRemoteUrl();
 
-        if (remote != null) {
-            remote = remoteUrl;
-        }
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
